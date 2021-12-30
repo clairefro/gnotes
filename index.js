498048132227d5dad730a8e3b6577c9c681aac2c
Claire Froelich <claire.froelich@gmail.com>
2021-12-30 15:09:44 -0500

    testing from docs

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const fs = require("fs");
 const path = require("path");
 const matter = require("gray-matter");
 const marked = require("marked");
+const pug = require("pug");
 
 const gnotesDir = path.resolve(__dirname, "gnotes");
 
-const testRaw = fs.readFileSync(path.resolve(gnotesDir, "test.md"), "utf-8");
+const testRaw = fs.readFileSync(path.resolve(gnotesDir, "notes1.md"), "utf-8");
 const test = matter(testRaw);
 
 console.log({ gnotesDir });
 console.log();
 console.log(marked.parse(test.content));
+
+const homepageHTML = pug.renderFile(
+  path.resolve(__dirname, "templates", "homepage.pug"),
+  {
+    content: test.content,
+  }
+);
+
+fs.writeFileSync(path.resolve(__dirname, "docs", "index.html"), homepageHTML);
 // get all md files in 'gnotes'
 // get 'top.md' (currently read, etc)
 
